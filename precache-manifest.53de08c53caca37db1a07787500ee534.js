2ab5a7e923c008ac17f6f0e2217c6dff90f65444
special3220 <special3220@yahoo.com>
2020-07-30 02:34:50 -0400

    Updates

---
+++ precache-manifest.53de08c53caca37db1a07787500ee534.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85e88e0177fdb070a91cb237755be553",
+    "url": "/seasons/index.html"
+  },
+  {
+    "revision": "ff9f180d65e2b3198dd4",
+    "url": "/seasons/static/css/main.78543caa.chunk.css"
+  },
+  {
+    "revision": "c2d246ecc2b83ccac6a6",
+    "url": "/seasons/static/js/2.8b77c9df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/seasons/static/js/2.8b77c9df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff9f180d65e2b3198dd4",
+    "url": "/seasons/static/js/main.e16c46a9.chunk.js"
+  },
+  {
+    "revision": "7aa38b62a80534558f86",
+    "url": "/seasons/static/js/runtime-main.53d2d0be.js"
+  }
+]);
